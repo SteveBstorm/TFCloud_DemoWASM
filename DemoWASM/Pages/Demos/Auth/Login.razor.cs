2ad2b30114df56ae8c2fbbd4e09a418ef89055e2
stevebstorm <steve.lorent@gmail.Com>
2024-10-10 16:59:23 +0200

    add login

---
+++ DemoWASM/Pages/Demos/Auth/Login.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Net.Http.Json;
+
+namespace DemoWASM.Pages.Demos.Auth
+{
+    public partial class Login
+    {
+        public LoginForm form { get; set; } = new LoginForm();
+
+        [Inject]
+        public HttpClient Client { get; set; }
+
+        [Inject]
+        public IJSRuntime JS { get; set; }
+
+        public async Task SubmitForm()
+        {
+            HttpResponseMessage response = 
+                await Client.PostAsJsonAsync("user/login", form);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                await Console.Out.WriteLineAsync("Erreur : " + response.ReasonPhrase);
+            }
+
+            string token = await response.Content.ReadAsStringAsync();
+            await JS.InvokeVoidAsync("localStorage.setItem", "token", token);
+
+        }
+    }
+}
